88495641029aa6306046d5d689cc0b19463328e2
Momendohu <pinog.jh@gmail.com>
2018-07-04 15:40:33 +0900

    プロジェクト作成 + タンクモデル + 床テクスチャ + Tankスクリプト追加

---
+++ Assets/Tank.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Tank : MonoBehaviour {
+    //=============================================================
+    private GameObject tower;
+
+	//=============================================================
+	private void Init(){
+		CRef();
+	}
+
+	//=============================================================
+	private void CRef(){
+        tower = GameObject.Find("Tank/tower");
+	}
+
+	//=============================================================
+	private void Awake () {
+		Init();
+	}
+
+	private void Start () {
+		
+	}
+	
+	private void Update () {
+        tower.transform.eulerAngles+=Time.deltaTime*Vector3.up*10;
+	}
+}
